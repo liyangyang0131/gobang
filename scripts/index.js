91bca2ae2f482052c6f681f4285eaa4ce49a9473
liyangyang0131 <394891653@qq.com>
2016-02-13 13:41:14 +0800

    init

--- scripts/index.js
+++ scripts/index.js
@@ -5,40 +5,46 @@ window.onload=function(){
 	var hidden = document.getElementById('hidden'),
 		who = document.getElementById('who'),
 		again = document.getElementById('again'),
-		cancle = document.getElementById('cancle');
+		cancle = document.getElementById('cancle'),
+		item1 = document.getElementById('item1'),
+		item2 = document.getElementById('item2');
 	var dic1={},dic2={};
-	var row = 20,
+	var row = 18,
 	    line,
-	    lin2,
+	    line2,
 	    div;
-	for(var i = 0;i<row;i++){
-		line = document.createElement('div');
-		line.style.width='600px';
-		line.style.borderBottom='1px solid #757575';
-		line.style.position='absolute';
-		line.style.top=(600/row)*i+(600/row)/2+'px';
-		sence.appendChild(line);
 
-		line2 = document.createElement('div');
-		line2.style.height='600px';
-		line2.style.borderLeft='1px solid #757575';
-		line2.style.position='absolute';
-		line2.style.left=(600/row)*i+(600/row)/2+'px';
-		sence.appendChild(line2);
-	}
+	var draw = function(){
+		for(var i = 0;i<row;i++){
+			line = document.createElement('div');
+			line.style.width='567px';
+			line.style.marginLeft="17px";
+			line.style.borderBottom='1px solid #757575';
+			line.style.position='absolute';
+			line.style.top=(600/row)*i+(600/row)/2+'px';
+			sence.appendChild(line);
 
-	for(i = 0;i<row;i++){
-		for(var j = 0;j<row;j++){
-			div=document.createElement('div');
-			div.setAttribute('class','block');
+			line2 = document.createElement('div');
+			line2.style.height='567px';
+			line2.style.marginTop="17px";
+			line2.style.borderLeft='1px solid #757575';
+			line2.style.position='absolute';
+			line2.style.left=(600/row)*i+(600/row)/2+'px';
+			sence.appendChild(line2);
+		}
 
-			div.setAttribute('id',i+'_'+j);
-
-			div.style.width=600/row+'px';
-			div.style.height=600/row+'px';
-			sence.appendChild(div);
+		for(i = 0;i<row;i++){
+			for(var j = 0;j<row;j++){
+				div=document.createElement('div');
+				div.setAttribute('class','block');
+				div.setAttribute('id',i+'_'+j);
+				div.style.width=600/row+'px';
+				div.style.height=600/row+'px';
+				sence.appendChild(div);
+			}
 		}
 	}
+	draw();
 
 	var isWin = function(id,dic){
 		var x = Number(id.split('_')[0]),
@@ -93,12 +99,18 @@ window.onload=function(){
 				this.style.boxShadow='2px 2px 8px #757575';
 				dic1[this.getAttribute('id')]=true;
 				if(isWin(id,dic1)){
-					// alert('黑棋完胜');
 					hidden.style.display='block';
-					who.innerHTML = '黑棋完胜!';
+					item1.style.background="url(images/hei.png) no-repeat";
+					item1.style.backgroundSize="contain";
+					item2.innerHTML="胜";
 					again.onclick = function(){
 						hidden.style.display='none';
 						location.reload();
+					/*	for(var i = 0;i<block.length;i++){
+							block[i].removeAttribute("hascolor");
+							block[i].style.cssText="width: 33.3333333333333px; height: 33.3333333333333px;";
+						}
+						kaiguan = true;*/
 					}
 					cancle.onclick = function(){
 						hidden.style.display='none';
@@ -114,12 +126,18 @@ window.onload=function(){
 				this.style.boxShadow='2px 2px 15px #e0e0e0 inset,2px 2px 8px #757575';
 				dic2[this.getAttribute('id')]=true;
 				if(isWin(id,dic2)){
-					// alert('白棋完胜');
 					hidden.style.display='block';
-					who.innerHTML = '白棋完胜!';
+					item1.style.background="url(images/bai.png) no-repeat";
+					item1.style.backgroundSize="contain";
+					item2.innerHTML="胜";
 					again.onclick = function(){
 						hidden.style.display='none';
 						location.reload();
+					/*	for(var i = 0;i<block.length;i++){
+							block[i].removeAttribute("hascolor");
+							block[i].style.cssText="width: 33.3333333333333px; height: 33.3333333333333px;";
+						}
+						kaiguan = true;*/
 					}
 					cancle.onclick = function(){
 						hidden.style.display='none';
@@ -129,8 +147,6 @@ window.onload=function(){
 			}
 			this.setAttribute('hasColor','true');
 		}
-
-
 		// document.onkeydown = function(e){
 		// 	var d = e.keyCode;
 		// 	console.log(d);
